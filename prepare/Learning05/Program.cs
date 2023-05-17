b3c8a8ed51d2ac6a961fdc8ab3a73da53f1a8d03
NauelGonzalez <nrgonzi@gmail.com>
2023-05-17 19:48:19 -0300

    corrected loop

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -10,13 +10,22 @@ class Program
         Circle c = new Circle("blue", 8);
 
 
-        List<Shape> l = new List<Shape>();
-        l.Append(s);
-        l.Append(r);
-        l.Append(c);
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(s);
+        shapeList.Add(r);
+        shapeList.Add(c);
 
-        foreach (Shape shap in l){
-                Console.WriteLine($"Square color: {shap.GetColor()}, area: {shap.GetArea().ToString()}");
+        Console.WriteLine(shapeList.Count());
+
+        for (int i = shapeList.Count()-1; i >= 0;i--){
+                Console.WriteLine($"Square color: {shapeList[i].GetColor()}, area: {shapeList[i].GetArea().ToString()} cm2");
+        }
+
+
+        Console.WriteLine();
+
+        foreach (Shape shap in shapeList){
+            Console.WriteLine($"Square color: {shap.GetColor()}, area: {shap.GetArea().ToString()} cm2");
         }
 
     }
