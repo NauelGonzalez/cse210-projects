5cbab938343aa34246abf206045225133e726199
NauelGonzalez <nrgonzi@gmail.com>
2023-05-17 19:36:06 -0300

    added loop

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,14 +6,18 @@ class Program
     {
         Console.WriteLine("Hello Learning05 World!");
         Square s = new Square("red", 6);
-        Console.WriteLine($"Square color: {s.GetColor()}, area: {s.GetArea().ToString()}");
-        
-
         Rectangle r = new Rectangle("green", 6,4);
-        Console.WriteLine($"Square color: {r.GetColor()}, area: {r.GetArea().ToString()}");
-
         Circle c = new Circle("blue", 8);
-        Console.WriteLine($"Square color: {c.GetColor()}, area: {c.GetArea().ToString()}");
+
+
+        List<Shape> l = new List<Shape>();
+        l.Append(s);
+        l.Append(r);
+        l.Append(c);
+
+        foreach (Shape shap in l){
+                Console.WriteLine($"Square color: {shap.GetColor()}, area: {shap.GetArea().ToString()}");
+        }
 
     }
 }
